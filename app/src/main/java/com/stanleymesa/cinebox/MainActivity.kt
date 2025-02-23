527870adbe285dfe405677eb23d9e26160cf4873
stanleymesa <stanleymesa2001@gmail.com>
2025-02-23 14:36:57 +0700

    hai dev

--- app/src/main/java/com/stanleymesa/cinebox/MainActivity.kt
+++ app/src/main/java/com/stanleymesa/cinebox/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android Dev hai hai")
+                    Greeting("Android Dev hai dev")
                 }
             }
         }
